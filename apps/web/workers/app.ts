4e50af9ce4c20fa49c40fb3481342ff513197019
kurage <git@umitsuki.dev>
2026-09-21 01:50:23 +0900

    fix(web): CDNキャッシュ制御をCloudflare内に限定

--- apps/web/workers/app.ts
+++ apps/web/workers/app.ts
@@ -17,12 +17,12 @@ export default {
 		const context = new RouterContextProvider();
 		context.set(cloudflareContext, { env, ctx });
 		return requestHandler(request, context).then(response => {
-			if (response.headers.has("Cache-Control")) {
+			if (response.headers.has("Cloudflare-CDN-Cache-Control")) {
 				return response;
 			}
 
 			const headers = new Headers(response.headers);
-			headers.set("Cache-Control", "no-store");
+			headers.set("Cloudflare-CDN-Cache-Control", "no-store");
 			return new Response(response.body, {
 				status: response.status,
 				statusText: response.statusText,
